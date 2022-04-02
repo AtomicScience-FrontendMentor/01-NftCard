4c95e5caf288ff2e5f90173deebebac75e1d8176
dmitry.greblyuk@mail.ru <dmitry.greblyuk@mail.ru>
2022-04-02 20:45:47 +0300

    Card base outline done

--- src/components/NftCard/NftCard.tsx
+++ src/components/NftCard/NftCard.tsx
@@ -3,6 +3,7 @@ import "../../styles/fonts.scss"
 import "../../styles/nftCard.scss"
 import NftImage from './NftImage';
 import NftStatusBar from "./NftStatusBar";
+import image from "../../icons/image-avatar.png";
 
 interface NftCardProps {
     
@@ -11,10 +12,14 @@ interface NftCardProps {
 const NftCard: React.FunctionComponent<NftCardProps> = () => {
     return <div className='nft-card'> 
         <NftImage onClick={() => {}}/>
-        <p className="title"> Equilibrium #3429 </p>
+        <a className="title" href="/"> Equilibrium #3429 </a>
         <p className="description"> Our Equilibrium collection promotes balance and calm. </p>
         <NftStatusBar/>
         <hr/>
+        <div className="attribution"> 
+          <img src={ image } className="avatar" alt="Author"/>
+          Creation of <a className="author" href="/">Jules Wyvern</a>
+        </div>
     </div>;
 }
  
